873ca5fbaea56839027dfe4f9c185b6ceba8c018
hamed.babaei <hamed.babaei>
2021-11-25 15:59:33 +0330

    update ngrams

--- models/ngram_ml.py
+++ models/ngram_ml.py
@@ -4,7 +4,8 @@
 """
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.calibration import CalibratedClassifierCV
 
 
 class NgramML:
@@ -14,10 +15,11 @@ class NgramML:
     def __init__(self, config):
         self.model = Pipeline([('TFIDF', TfidfVectorizer(ngram_range=config.ngram_range,
                                                          stop_words=config.stop_words,
-                                                         sublinear_tf=config.sublinear_tf)), 
-                               ('ML', RandomForestClassifier(n_estimators=config.n_estimators))])
+                                                         sublinear_tf=config.sublinear_tf,
+                                                         analyzer=config.analyzer)),
+                               ("ML",  CalibratedClassifierCV(LinearSVC()))])
 
-    def fit(self, X:list, y:list):
+    def fit(self, X: list, y: list):
         self.model.fit(X, y)
 
     def predict(self, X: list) -> list:
