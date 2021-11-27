874a9f5355f2600b91c72f95b6e5d31f37c8e753
Hamed Babaei <hamedbabaeigiglou@gmail.com>
2021-11-27 06:33:54 +0330

    fix typos

--- ml_runner.py
+++ ml_runner.py
@@ -4,7 +4,7 @@
 import os
 from configurations import BaseConfig
 from models import NgramML
-from models import twitter_train_model, twitter_test_model
+from models import ml_twitter_train_model, ml_twitter_test_model
 from datahandlers import DataReader
 import random
 import warnings
@@ -13,16 +13,15 @@ warnings.filterwarnings('ignore')
 
 if __name__ == '__main__':
     CONFIG = BaseConfig().get_args()
-
     random.seed(CONFIG.seed_num)
 
     if not CONFIG.test:
         MODEL = NgramML(CONFIG)
-        twitter_train_model(MODEL, CONFIG)
+        ml_twitter_train_model(MODEL, CONFIG)
     else:
         MODEL = DataReader.load_pkl(os.path.join(CONFIG.pre_trained_dir, CONFIG.model_name, CONFIG.model_name+".sav"))
         if CONFIG.dataset == 'twitter':
-            twitter_test_model(MODEL, CONFIG)
+            ml_twitter_test_model(MODEL, CONFIG)
         else:
             pass
 
